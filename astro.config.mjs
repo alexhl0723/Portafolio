a8f5e9973c49672025efb598b39587006e078afc
alexHL0723 <alexrodrigoherbas07@gmail.com>
2025-03-26 12:09:11 -0500

    para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-export default defineConfig({});
+import vercel from "@astrojs/vercel/serverless"
+
+export default defineConfig({
+
+  output: "server",
+
+  integrations: [tailwind()],
+  adapter: vercel(),
+});
---
+++ src/content/info.ts
@@ -0,0 +1,35 @@
+export const proys = {
+    projects: [
+        {
+            name: "Nautica Plus",
+            description: "Aplicación web que permite a los usuarios registrar una reserva y gestionar sus reservas.",
+            image: "/paisaje.jpg",
+            github: "https://github.com/alexhl0723/ReservaRestaurante",
+            technologies: [
+                { name: "Java", bgColor: "rgba(253, 230, 138, 0.1)", textColor: "#FDE68A" },
+                { name: "Tailwind CSS", bgColor: "rgba(191, 219, 254, 0.1)", textColor: "#BFDBFE" },
+                { name: "MySQL", bgColor: "rgba(21, 128, 61, 0.2)", textColor: "#BFDBFE" }
+            ]
+        },
+        {
+            name: "Bot Discord",
+            description: "Bot de Discord creado para hacer más divertida la experiencia con amigos en Discord.",
+            image: "/paisaje.jpg",
+            github: "https://github.com/tu-repo-bot",
+            technologies: [
+                { name: "JavaScript", bgColor: "rgba(253, 230, 138, 0.1)", textColor: "#FDE68A" },
+                { name: "Node.js", bgColor: "rgba(191, 219, 254, 0.1)", textColor: "#BFDBFE" }
+            ]
+        },
+        {
+            name: "Librería BookStore",
+            description: "Aplicación web que permite a los usuarios buscar libros y agregarlos a su carrito de compras.",
+            image: "/paisaje.jpg",
+            github: "https://github.com/tu-repo-bookstore",
+            technologies: [
+                { name: "Astro", bgColor: "rgba(253, 230, 138, 0.1)", textColor: "#FDE68A" },
+                { name: "Tailwind CSS", bgColor: "rgba(191, 219, 254, 0.1)", textColor: "#BFDBFE" }
+            ]
+        }
+    ]
+};
